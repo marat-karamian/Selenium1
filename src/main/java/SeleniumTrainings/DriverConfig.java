d1381916ba51bee198b693a8f1cc984a1c28d072
marat-karamian <marat_karamian@epam.com>
2018-10-17 11:37:25 +0300

    Class HomeWork1 added

--- src/main/java/SeleniumTrainings/DriverConfig.java
+++ src/main/java/SeleniumTrainings/DriverConfig.java
@@ -3,12 +3,12 @@ package SeleniumTrainings;
 public class DriverConfig {
 
     public void chrome() {
-        String exepath = "C:\\Users\\mkara\\Documents\\JavaMP\\chromedriver.exe";
+        String exepath = "C:\\Users\\Marat_Karamian\\Documents\\JavaMP\\chromedriver.exe";
         System.setProperty("webdriver.chrome.driver",exepath);
     }
 
     public void firefox() {
-        String exepath = "C:\\Users\\mkara\\Documents\\JavaMP\\geckodriver.exe";
+        String exepath = "C:\\Users\\Marat_Karamian\\Documents\\JavaMP\\geckodriver.exe";
         System.setProperty("webdriver.gecko.driver", exepath);
     }
 }
---
+++ src/test/java/SeleniumTrainings/HomeWork1.java
@@ -0,0 +1,61 @@
+package SeleniumTrainings;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.concurrent.TimeUnit;
+
+public class HomeWork1 {
+
+    static WebDriver driver;
+
+    @Before
+    public void setDriver() {
+        DriverConfig driverConfig = new DriverConfig();
+        driverConfig.chrome();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-notifications");
+        driver = new ChromeDriver(options);
+    }
+
+    @Test
+    public void hw1() {
+        driver.manage().window().maximize();
+        driver.get("https://facebook.com");
+
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        WebElement email = driver.findElement(By.id("email"));
+        email.clear();
+        email.sendKeys("mkaramyan.dp3@gmail.com");
+
+        WebElement pass = driver.findElement(By.id("pass"));
+        pass.clear();
+        pass.sendKeys("TestPassword");
+
+        driver.findElement(By.id("loginbutton")).click();
+
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        //Checks if user is logged in
+
+        boolean isLogged = driver.findElement(By.xpath("//*[@id=\"navItem_4748854339\"]/a/div")).isDisplayed();
+
+        if (isLogged) {
+            System.out.println("The user is logged in");
+        } else {
+            System.out.println("The user is not logged in");
+        }
+    }
+
+    @After
+    public void quit() {
+        driver.quit();
+    }
+}
